317269a2c739e2fac00b319e2d68ca5c38d61cc9
Naresh Podichetty <nareshpodi@hotmail.com>
2017-08-02 23:32:53 +0530

    Project complete!

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,9 +1,14 @@
 package com.londonappbrewery.magiceightball;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.StateListDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -27,11 +32,17 @@ public class MainActivity extends AppCompatActivity {
         ballArray.add(R.drawable.ball4);
         ballArray.add(R.drawable.ball5);
 
-        Button myButton = (Button) findViewById(R.id.button_ask);
+        final Button myButton = (Button) findViewById(R.id.button_ask);
         myButton.setOnClickListener(new View.OnClickListener() {
+
+            //AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.5F);
 
             @Override
             public void onClick(View v) {
+                //v.startAnimation(buttonClick);
+
+                //addClickEffect(myButton);
+
                 Random randomNumberGenerator = new Random();
                 int number  = randomNumberGenerator.nextInt(5);
                 Log.d("Image id", "Image number is " + number);
@@ -39,5 +50,19 @@ public class MainActivity extends AppCompatActivity {
                 ballDisplay.setImageResource(ballArray.get(number));
             }
         });
+    }
+
+    void addClickEffect(View view) {
+        Drawable drawableNormal = view.getBackground();
+
+        Drawable drawablePressed = view.getBackground().getConstantState().newDrawable();
+        drawablePressed.mutate();
+        drawablePressed.setColorFilter(Color.argb(50, 0, 0, 0), PorterDuff.Mode.SRC_ATOP);
+
+        StateListDrawable listDrawable = new StateListDrawable();
+        listDrawable.addState(new int[] {android.R.attr.state_pressed}, drawablePressed);
+        listDrawable.addState(new int[] {android.R.attr.defaultValue}, drawableNormal);
+
+        view.setBackground(listDrawable);
     }
 }
